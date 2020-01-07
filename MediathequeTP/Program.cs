b1ce3f6d7b09a2e21aae218d43dc8b427ae1be54
SamNord <samira.boudiaf59@gmail.com>
2020-01-07 20:29:49 +0100

    Ajout solution tp

---
+++ MediathequeTP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MediathequeTP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
